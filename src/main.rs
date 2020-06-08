1d96294a9ab62dbb12318862a8cdfdc284eb409f
Aaabot <andra.antariksa+bot@gmail.com>
2020-06-08 11:46:31 +0000

    Renew md5sum of latest release

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod ui;
 mod updater;
 
 #[allow(clippy::unreadable_literal)]
-const CURRENT_VERSION: u64 = 20200607;
+const CURRENT_VERSION: u64 = 20200608;
 const REPOSITORY_URL: &str = "https://github.com/bebasid/bebasin";
 const LATEST_VERSION_URL: &str =
     "https://raw.githubusercontent.com/bebasid/bebasin/master/latest.json";
